aebe31c8e1fdc1fddec0b090663995eb48e7d359
Ricardo Montoya <loquendo3277@gmail.com>
2022-06-13 14:23:07 -0500

    feat: Creating the calculator project without functionality

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         alt = "logo" / >
         <
         p >
-        Edit < code > src / App.js < /code> and save to reload. <
+        Edit < code > src / App.js < /code> and save to reload . <
         /p> <
         a className = "App-link"
         href = "https://reactjs.org"
