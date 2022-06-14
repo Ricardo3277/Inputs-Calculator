876458fae2919e8727897253cf1834422396439c
Ricardo Montoya <loquendo3277@gmail.com>
2022-06-13 23:10:58 -0500

    docs: Creation of the CSS without code and modification to the JS in the buttons

--- src/App.js
+++ src/App.js
@@ -19,15 +19,16 @@ const App = () => {
         <
         button > Clear < /button> <
         button > C < /button> <
-        button > & divide; < /button> <
+        button > /</button >
+        <
         button > 7 < /button> <
         button > 8 < /button> <
         button > 9 < /button> <
-        button > & times; < /button> <
+        button > x < /button> <
         button > 4 < /button> <
         button > 5 < /button> <
         button > 6 < /button> <
-        button > & andash; < /button> <
+        button > - < /button> <
         button > 1 < /button> <
         button > 2 < /button> <
         button > 3 < /button> <
